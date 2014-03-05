e221e02ae16bca7b4ee90aeda9b03c60c8d6f1fe
John Goodall <jgoodall@gmail.com>
2014-03-05 10:50:10 -0500

    revert to use null encoding for pdf file - no support for node 0.8

--- test/route-test.js
+++ test/route-test.js
@@ -21,7 +21,7 @@ var getTestOneKey = 'testJson12'
   , getTestTwoData = '<xml><test>a test</test></xml>'
   , getTestThreeKey = 'testPdf12'
   , getTestThreeFile = path.join(__dirname, 'fixtures', 'test.pdf')
-  , getTestThreeData = fs.readFileSync(getTestThreeFile);
+  , getTestThreeData = fs.readFileSync(getTestThreeFile, {encoding: null});
 
 
 describe('Test routes (' + url + ')', function () {
